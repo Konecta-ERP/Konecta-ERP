d828a49742f7c8c1cc4bc812edfdff04950a447e
omarse7a <omarsamehcs@gmail.com>
2025-10-28 23:26:31 +0300

    chore: add frontend files

---
+++ frontend/ERP/src/app/app.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ERP');
+
+  friends = ['Alice', 'Bob', 'Charlie'];
+}
---
+++ frontend/ERP/src/app/components/auth-nav/auth-nav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-nav',
+  imports: [],
+  templateUrl: './auth-nav.html',
+  styleUrl: './auth-nav.css',
+})
+export class AuthNav {
+
+}
