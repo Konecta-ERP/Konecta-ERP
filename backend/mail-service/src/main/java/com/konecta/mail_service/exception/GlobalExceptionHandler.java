304f6da76820e16468e77740fd1533a7feb449b5
omarse7a <omarsamehcs@gmail.com>
2025-11-13 18:13:13 +0200

    chore: add the mail service boilerplate

---
+++ backend/mail-service/src/main/java/com/konecta/mail_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.konecta.mail_service.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
